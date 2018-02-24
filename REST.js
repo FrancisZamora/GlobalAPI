f6c540a515790816570a653187c392b7809aa3af
FrancisZamora <franciszamor@gmail.com>
2018-02-24 16:58:55 -0500

    connection to database, e, sign up route

---
+++ REST.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+
+   
+
+   router.post("/signup",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+        var table = ["user_login","user_email","user_password",req.body.email,md5(req.body.password)];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Success" : true,  "Message" : "User Added !"});
+            }
+        });
+    });
+
+
+
+
+
+
+
+
+}
+
+module.exports = REST_ROUTER;
