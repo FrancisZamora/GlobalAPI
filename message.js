12c8e9753fb240482e89ee7767ce3eca03017950
FrancisZamora <franciszamor@gmail.com>
2018-04-01 13:46:28 -0400

    create chatroom

--- message.js
+++ message.js
@@ -6,7 +6,22 @@ function REST_ROUTER(router,connection,md5) {
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
 
+	   router.post("/message/createchat",function(req,res){
 
+        var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
+        var table = ["chatroom","min_id","max_id","subject",req.body.min_id,req.body.max_id, req.body.subject];
+      
+
+        query = mysql.format(query,table);
+        console.log(query);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query: signing up "});
+            } else {
+                res.json({"Success" : true,  "Message" : "Chat Created!"});
+            }
+        });
+      });
 
 
 
