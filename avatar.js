0de6e022bbce66b7c14f2bf26eeaad4c69b1136a
FrancisZamora <franciszamor@gmail.com>
2018-04-07 21:21:40 -0400

    waiting for aws keys

--- avatar.js
+++ avatar.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var AWS = require('aws-sdk');
+var new AWS.S3();
 function REST_ROUTER(router,connection,md5) {
     var self = this;
     self.handleRoutes(router,connection,md5);
 }
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
-
-	   router.post("/message/createchat",function(req,res){
+       router.post("/avatar/uploadImage",function(req,res){
+        var bucketParams = {Bucket: String(req.user_image)};
+        s3.createBucket(bucketParams);
+        var s3Bucket = new AWS.S3( { params: {Bucket: 'myBucket'} } )
 
         var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
         var table = ["chatroom","min_id","max_id","subject",req.body.min_id,req.body.max_id, req.body.subject];
@@ -23,23 +27,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
         });
       });
 
-	   router.post("/message/sendmessage",function(req,res){
-
-        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
-        var table = ["messages","id","message",req.body.id,req.body.message];
-      
-
-        query = mysql.format(query,table);
-        console.log(query);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query: Sending Message"});
-            } else {
-                res.json({"Success" : true,  "Message" : "Chat Created!"});
-            }
-        });
-      });
-
+	  
 
 
 
