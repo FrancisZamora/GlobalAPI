9e141d31fdd52dbf467a23ada7c9e3c72a5c097b
FrancisZamora <franciszamor@gmail.com>
2018-04-15 19:32:30 -0400

    create review

--- review.js
+++ review.js
@@ -6,26 +6,10 @@ function REST_ROUTER(router,connection,md5) {
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
 
-	   router.post("/message/createchat",function(req,res){
 
-        var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
-        var table = ["chatroom","min_id","max_id","subject",req.body.min_id,req.body.max_id, req.body.subject];
-      
+       router.get("/reviews/getall/:reviewed_id",function(req,res){
 
-        query = mysql.format(query,table);
-        console.log(query);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query: Creating Chat"});
-            } else {
-                res.json({"Success" : true,  "Message" : "Chat Created!"});
-            }
-        });
-      });
-
-       router.get("/reviews/rating/:reviewed_id",function(req,res){
-
-        var query = "SELECT ?? FROM ?? WHERE ?? = ? ";
+        var query = "SELECT * FROM ?? WHERE ?? = ? ";
         var table = ["rating","reviews ",req.params.reviewed_id];
 
 
@@ -36,6 +20,25 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
                 res.json({"Error" : true, "Message" : "Error executing MySQL query: Retreiving Rating"});
             } else {
                 res.json({"Success" : true,  "Message" : "Rating Retrieved"});
+            }
+        });
+      });
+
+
+      router.post("/reviews/createreview",function(req,res){
+
+        var query = "INSERT INTO ??(??,??,??,??) VALUES (?,?,?,?)";
+        var table = ["reviews","reviewer_id","reviewed_id","review_text","rating",req.body.reviewer_id,req.body.reviewed_id, req.body.text,req.body.rating];
+       
+      
+
+        query = mysql.format(query,table);
+        console.log(query);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query: Creating Review"});
+            } else {
+                res.json({"Success" : true,  "Message" : "Review Created"});
             }
         });
       });
